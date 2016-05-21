e01e0d7b1f3bb5d54d428c8f237b72a3c5170b7d
Ian Fieldhouse <me@ianfieldhouse.com>
2016-05-21 18:29:21 +0100

    Add tests for number 0 to 9

--- number_to_words_test.py
+++ number_to_words_test.py
@@ -9,5 +9,16 @@ class TestNumberToWords(unittest.TestCase):
     def tearDown(self):
         self.n2w = None
 
+    def test_zero_and_single_digits(self):
+        NUMBERS = {
+            0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+            6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'
+        }
+        self.assert_numbers_equal_to_strings(NUMBERS)
+
+    def assert_numbers_equal_to_strings(self, numbers):
+        for number, string in numbers.iteritems():
+            self.assertEqual(string, self.n2w.convert(number))
+
 if __name__ == '__main__':
     unittest.main()
