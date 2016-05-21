8be4a44e8b65d502715c1903d1e45f8ba281bf32
Ian Fieldhouse <me@ianfieldhouse.com>
2016-05-21 16:38:06 +0100

    Add initial class definition and documentation

---
+++ number_to_words.py
@@ -0,0 +1,5 @@
+class NumberToWords(object):
+    """
+    Class for converting positive integer values to a textual representation
+    of the submitted number for value of 0 up to 999999999.
+    """
