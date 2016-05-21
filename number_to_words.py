477c02946b05c80a11bad6c9c20464ee2e82eab4
Ian Fieldhouse <me@ianfieldhouse.com>
2016-05-21 16:40:33 +0100

    Add constants
    max value and lists containing the necessary words for displaying all
    permutations of words

--- number_to_words.py
+++ number_to_words.py
@@ -3,3 +3,12 @@ class NumberToWords(object):
     Class for converting positive integer values to a textual representation
     of the submitted number for value of 0 up to 999999999.
     """
+
+    MAX = 999999999
+    SMALL_NUMBERS = ['', 'one', 'two', 'three', 'four', 'five', 'six',
+                     'seven', 'eight', 'nine', 'ten', 'eleven',
+                     'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+                     'seventeen', 'eighteen', 'nineteen']
+    TENS = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy',
+            'eighty', 'ninety']
+    LARGE_NUMBERS = ['', 'thousand', 'million']
